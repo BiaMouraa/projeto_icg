0f74a3d13596b7246a7a27f7d2c7829b2b285ae1
BiaMouraa <beatriz.moura3@academico.ufpb.br>
2025-04-22 18:42:31 -0300

    add: labirinto 2d

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # projeto_icg
+
+## lab_2d
+- compilação: gcc main.c Labirinto.c Grafo.c -o app -lGL -lGLU -lglut -lm
+- execução: ./app
+- monta um labirinto a partir de uma árvore geradora mínima
+    - ao executar é possível ver a árvore geradora, as paredes do labirinto e o menor caminho
---
+++ lab_2d/main.c
@@ -0,0 +1,73 @@
+#include <GL/glut.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "Labirinto.h"
+
+int tipo = 0;
+LABIRINTO *lab = NULL;
+
+// Função de desenho
+void display(void) {
+    glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glPushMatrix();
+    desenha_labirinto(lab, tipo);
+    glPopMatrix();
+
+    glutSwapBuffers();
+}
+
+// Atualização com tempo (opcional se precisar animação)
+void timer(int value) {
+    glutPostRedisplay();          // força redesenho
+    glutTimerFunc(10, timer, 0);  // agenda próxima chamada
+}
+
+// Interação com teclado
+void keyboard(unsigned char key, int x, int y) {
+    switch (key) {
+        case 27:  // ESC
+            destroi_labirinto(lab);
+            exit(0);
+            break;
+
+        case ' ':  // Espaço
+            tipo = (tipo + 1) % 4;
+            if (tipo > 3)
+                inicia_animacao(lab);
+            break;
+    }
+
+    glutPostRedisplay();
+}
+
+// Inicialização do contexto e labirinto
+void init(void) {
+    srand(time(NULL));
+    lab = monta_labirinto();
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(-1, 1, -1, 1); // define coordenadas de visualização
+    glMatrixMode(GL_MODELVIEW);
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowSize(650, 650);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Labirinto com GLUT");
+
+    init();
+
+    glutDisplayFunc(display);
+    glutKeyboardFunc(keyboard);
+    glutTimerFunc(10, timer, 0);
+
+    glutMainLoop();
+
+    return 0;
+}
